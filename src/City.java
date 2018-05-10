83f5c9f7f2f36015f17aa11485ff57cd9005eb38
captuayo <fg.captuayo@uniandes.edu.co>
2018-05-09 23:42:28 -0500

    send initial project

---
+++ src/City.java
@@ -0,0 +1,22 @@
+
+import java.awt.Point;
+
+public class City {
+
+    private Point CityPosition;
+    private String cityName;
+
+    City(String name, Point coordinate) {
+        cityName = name;
+        CityPosition = coordinate;
+    }
+
+    String getCityName() {
+        return cityName;
+    }
+
+    Point getCityPosition() {
+        return CityPosition;
+    }
+
+}
